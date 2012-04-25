985598c3ddce6855782c083dfdbc56e2d5cb8910
Jace Ju <jaceju@gmail.com>
2012-04-25 11:44:39 +0800

    add create command

---
+++ src/BBGen/Command/Create.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace BBGen\Command;
+
+use CLIFramework\Command;
+use CLIFramework\CommandInterface;
+use Exception;
+
+class CreateCommand extends Command implements CommandInterface
+{
+    public function execute($name)
+    {
+
+    }
+}
